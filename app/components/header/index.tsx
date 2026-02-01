516fdccfab1ba24bb5525e47af3b0220da949006
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2026-02-01 19:03:46 -0300

    first commit

---
+++ app/components/header/index.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import Image from "next/image"
+import Link from "next/link"
+import { NavItem } from "./nav-item"
+
+const NAV_ITEMS = [
+    {
+        label: 'Home',
+        href: '/'
+    },
+    {
+        label: 'Projetos',
+        href: '/projetos'
+    }
+]
+
+export const Header = () => {
+    return (
+        <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center">
+            {/* z-10: controla a ordem de empilhamento (eixo Z) dos elementos que se sobrepõem */}
+            {/* h-24: altura fixa de 24 unidades (equivalente a 6rem) */}
+            <div className="container flex items-center justify-between">
+                <Link href="/">
+                    <Image
+                    width={58}
+                    height={49}
+                    src="/images/logo.svg"
+                    alt="Logo Mateus Dev"
+                    />
+                </Link>
+
+                <nav className="flex items-center gap-4 sm:gap-10"> 
+                    {/* gap-10 quando a tela for maior que sm, se não, será gap-4 */}
+                    {NAV_ITEMS.map((item) => (
+                        // <NavItem key={item.href} label={item.label} href={item.href} />
+                        <NavItem key={item.href} {...item} />
+                        // Destructuring acima é equivalente ao comentário de cima
+                    ))}
+                </nav>
+            </div>
+        </header>
+    )
+}
