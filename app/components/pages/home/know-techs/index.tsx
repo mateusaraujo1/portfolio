48d43b397170ef5e6d9eb9c903b997f2b92a8156
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2026-02-02 11:15:52 -0300

    known tech

---
+++ app/components/pages/home/know-techs/index.tsx
@@ -0,0 +1,24 @@
+import { SectionTitle } from "@/app/components/section-title"
+import { KnownTech } from "./known-tech"
+import { TbBrandNextjs } from "react-icons/tb"
+
+export const KnownTechs = () => { 
+    return (
+        <section className="container py-16">
+            <SectionTitle 
+                title="Conhecimentos" 
+                subtitle="competências" />
+
+            <div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
+                {/* se repete até um valor x */}
+                {Array.from({ length: 8 }).map((_, index) => (
+                    <KnownTech key={index} tech={{
+                        icon: <TbBrandNextjs/>,
+                        name: 'TypeScript',
+                        startDate: '2020-01-01'
+                    }}/>
+                ))}
+            </div>
+        </section>
+    )
+}
