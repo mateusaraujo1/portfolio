3697299ffdca37ae20fd57923297364c0ccd5457
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2026-02-07 14:44:12 -0300

    view images project details

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,9 +1,11 @@
 import { PorjectDetails } from "@/app/components/pages/project/project-details";
+import { PorjectSections } from "@/app/components/pages/project/project-sections";
 
 export default function Project() {
     return (
         <>
-            <PorjectDetails/>
+            <PorjectDetails />
+            <PorjectSections />
         </>
     )
 }
