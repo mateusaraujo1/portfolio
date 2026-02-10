eeb30a66c647792cae98a753f05e6e352f62e311
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2026-02-10 11:48:50 -0300

    conect api highlight projects

--- app/components/pages/home/highlighted-projects/index.tsx
+++ app/components/pages/home/highlighted-projects/index.tsx
@@ -3,8 +3,13 @@ import { SectionTitle } from "@/app/components/section-title"
 import { ProjectCard } from "./project-card"
 import { Link } from "@/app/components/link"
 import { HiArrowNarrowRight } from "react-icons/hi"
+import { Project } from "@/app/types/projects"
 
-export const HighlightedProjects = () => {
+type HighlightProjectsProps = {
+  projects: Project[]
+}
+
+export const HighlightedProjects = ({ projects }: HighlightProjectsProps) => {
   return (
     <section className="container py-16">
         <SectionTitle 
@@ -14,12 +19,12 @@ export const HighlightedProjects = () => {
         <HorizontalDivider className="mb-16"/>
 
         <div>
-            <ProjectCard/>
-            <HorizontalDivider className="my-16"/>
-            <ProjectCard/>
-            <HorizontalDivider className="my-16"/>
-            <ProjectCard/>
-            <HorizontalDivider className="my-16"/>
+            {projects?.map(project => (
+              <div key={project.slug}>
+                <ProjectCard project={project} />
+                <HorizontalDivider className="my-16" />
+              </div>
+            ))}
 
             <p className="flex items-center gap-1.5">
               <span className="text-gray-400">Se interessou?</span>
--- app/components/pages/home/highlighted-projects/project-card/index.tsx
+++ app/components/pages/home/highlighted-projects/project-card/index.tsx
@@ -1,17 +1,22 @@
 import { Link } from "@/app/components/link"
 import { TechBadge } from "@/app/components/tech-badge"
+import { Project } from "@/app/types/projects"
 import Image from "next/image"
 import { HiArrowNarrowRight } from "react-icons/hi"
 
-export const ProjectCard = () => {
+type ProjectCardProps = {
+    project: Project;
+}
+
+export const ProjectCard = ({ project }: ProjectCardProps) => {
     return (
         <div className="flex gap-6 lg:gap-12 flex-col lg:flex-row">
             <div className="w-full h-full">
                 <Image
                     width={420}
                     height={304}
-                    src="https://media.graphassets.com/FRhUdgUQTHmLmwf9u0BA"
-                    alt="Thumbnail do projeto Bookwise"
+                    src={project.thumbnail.url}
+                    alt={`Thumbnail do projeto ${project.title}`}
                     className="w-full h-[200px] sm:h-[300px] lg:w-[420px] lg:min-h-full object-cover rounded-lg"
                 />
             </div>
@@ -24,21 +29,18 @@ export const ProjectCard = () => {
                         height={20} 
                         alt="Ícone de projeto"
                     />
-                    BookWise
+                    {project.title}
                 </h3>
 
-                <p className="text-gray-400 my-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores necessitatibus harum quis, quia voluptate repudiandae, accusantium minus veniam ducimus voluptatem odit? Obcaecati nostrum quod magnam alias officiis! Voluptatem, suscipit sit!</p>
+                <p className="text-gray-400 my-6">{project.shortDescription}</p>
 
                 <div className="flex gap-x-2 gap-y-3 flex-wrap mb-8 lg:max-w-[350px]">
-                    <TechBadge name="Next.js"/>
-                    <TechBadge name="Next.js"/>
-                    <TechBadge name="Next.js"/>
-                    <TechBadge name="Next.js"/>
-                    <TechBadge name="Next.js"/>
-                    <TechBadge name="Next.js"/>
+                    {project.technologies.map(tech => (
+                        <TechBadge key={`${project.title}-tech-${tech.name}`} name={tech.name}/>
+                    ))}
                 </div>
 
-                <Link href="/projects/bookwise">
+                <Link href={`/projects/${project.slug}`}>
                     Ver projeto
                     <HiArrowNarrowRight/>
                 </Link>
