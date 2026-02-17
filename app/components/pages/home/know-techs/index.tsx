cad3c04dfd997851fc4aeaad7ec2331cb470c30b
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2026-02-17 17:23:52 -0300

    motion for KnownTechs

--- app/components/pages/home/know-techs/index.tsx
+++ app/components/pages/home/know-techs/index.tsx
@@ -1,7 +1,9 @@
+"use client"
+
 import { SectionTitle } from "@/app/components/section-title"
 import { KnownTech } from "./known-tech"
-import { TbBrandNextjs } from "react-icons/tb"
 import { KnownTech as IKnownTech } from "@/app/types/projects"
+import { motion } from 'framer-motion';
 
 type KnownTechsProps = {
     techs: IKnownTech[]
@@ -17,8 +19,16 @@ export const KnownTechs = ({ techs }: KnownTechsProps) => {
 
             <div className="grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
                 {/* se repete até um valor x */}
-                {techs?.map((tech) => (
-                    <KnownTech key={tech.name} tech={tech}/>
+                {techs?.map((tech, index) => (
+                    <motion.div 
+                        key={tech.name}
+                        initial={{ opacity: 0, x: -100 }}
+                        animate={{ opacity: 1, x: 0 }}
+                        exit={{ opacity: 0, x: -100 }}
+                        transition={{ duration: 0.15, delay: index * 0.1 }}
+                    >
+                        <KnownTech tech={tech}/>
+                    </motion.div>
                 ))}
             </div>
         </section>
