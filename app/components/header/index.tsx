37b39d54281000fc8106246f195e979e163a04d2
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2026-02-17 16:48:37 -0300

    motion in navbar

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import { NavItem } from "./nav-item"
+import { motion } from 'framer-motion';
 
 const NAV_ITEMS = [
     {
@@ -17,7 +18,12 @@ const NAV_ITEMS = [
 
 export const Header = () => {
     return (
-        <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center">
+        <motion.header 
+            className="absolute top-0 w-full z-10 h-24 flex items-center justify-center"
+            initial={{ top: -100 }}
+            animate={{ top: 0 }}
+            transition={{ duration: 0.5 }}
+        >
             {/* z-10: controla a ordem de empilhamento (eixo Z) dos elementos que se sobrepõem */}
             {/* h-24: altura fixa de 24 unidades (equivalente a 6rem) */}
             <div className="container flex items-center justify-between">
@@ -40,6 +46,6 @@ export const Header = () => {
                     ))}
                 </nav>
             </div>
-        </header>
+        </motion.header>
     )
 }
