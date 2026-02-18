0a41585c42f0b2839ce9aee2b0715a068a1688eb
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2026-02-18 12:09:29 -0300

    motion for project card

--- app/components/pages/home/highlighted-projects/project-card/index.tsx
+++ app/components/pages/home/highlighted-projects/project-card/index.tsx
@@ -1,17 +1,38 @@
+"use client"
+
 import { Link } from "@/app/components/link"
 import { TechBadge } from "@/app/components/tech-badge"
 import { Project } from "@/app/types/projects"
 import Image from "next/image"
 import { HiArrowNarrowRight } from "react-icons/hi"
+import { motion } from "framer-motion"
 
 type ProjectCardProps = {
     project: Project;
 }
 
 export const ProjectCard = ({ project }: ProjectCardProps) => {
+    const animProps = {
+        initial: { opacity: 0, y: 50 },
+        whileInView: { opacity: 1, y: 0 },
+        exit: { opacity: 0, y: 50 }
+    }
+
     return (
-        <div className="flex gap-6 lg:gap-12 flex-col lg:flex-row">
-            <div className="w-full h-[200px] sm:h-[300px] lg:w-[420px] lg:min-h-full">
+        <motion.div 
+            className="flex gap-6 lg:gap-12 flex-col lg:flex-row"
+            initial={{ opacity: 0, y: 100 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 100 }}
+            transition={{ duration: 0.5 }}
+        >
+            <motion.div 
+                className="w-full h-[200px] sm:h-[300px] lg:w-[420px] lg:min-h-full"
+                initial={{ opacity: 0, y: 100, scale: 0.5 }}
+                whileInView={{ opacity: 1, y: 0, scale: 1 }}
+                exit={{ opacity: 0, y: 100, scale: 0.5 }}
+                transition={{ duration: 0.5, delay: 0.3 }}
+            >
                 <Image
                     width={420}
                     height={304}
@@ -19,10 +40,14 @@ export const ProjectCard = ({ project }: ProjectCardProps) => {
                     alt={`Thumbnail do projeto ${project.title}`}
                     className="w-full h-full object-cover rounded-lg"
                 />
-            </div>
+            </motion.div>
 
             <div className="flex-1 lg:py-[18px]">
-                <h3 className="flex items-center gap-3 font-medium text-lg text-gray-50">
+                <motion.h3 
+                    className="flex items-center gap-3 font-medium text-lg text-gray-50"
+                    {...animProps}
+                    transition={{ duration: 0.7 }}
+                >
                     <Image 
                         src="/images/icons/project-title-icon.svg" 
                         width={20} 
@@ -30,13 +55,26 @@ export const ProjectCard = ({ project }: ProjectCardProps) => {
                         alt="Ícone de projeto"
                     />
                     {project.title}
-                </h3>
+                </motion.h3>
 
-                <p className="text-gray-400 my-6">{project.shortDescription}</p>
+                <motion.p 
+                    className="text-gray-400 my-6"
+                    {...animProps}
+                    transition={{ duration: 0.2, delay: 0.3 }}
+                >
+                    {project.shortDescription}
+                </motion.p>
 
                 <div className="flex gap-x-2 gap-y-3 flex-wrap mb-8 lg:max-w-[350px]">
-                    {project.technologies.map(tech => (
-                        <TechBadge key={`${project.title}-tech-${tech.name}`} name={tech.name}/>
+                    {project.technologies.map((tech, index) => (
+                        <TechBadge 
+                            key={`${project.title}-tech-${tech.name}`} 
+                            name={tech.name}
+                            initial={{ opacity: 0, scale: 1 }}
+                            whileInView={{ opacity: 1, scale: 1 }}
+                            exit={{ opacity: 0, scale: 0 }}
+                            transition={{ duration: 0.2, delay: 0.5 + index * 0.1}}
+                        />
                     ))}
                 </div>
 
@@ -46,6 +84,6 @@ export const ProjectCard = ({ project }: ProjectCardProps) => {
                 </Link>
 
             </div>
-        </div>
+        </motion.div>
     )
 }
