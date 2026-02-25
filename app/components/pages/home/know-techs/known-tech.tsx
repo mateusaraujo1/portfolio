68046e5d6c58c4dd1ee5ee9d48395e48452893a9
mateusaraujo1 <f.mateusaraujo2@gmail.com>
2026-02-25 11:00:23 -0300

    rm: comments

--- app/components/pages/home/know-techs/known-tech.tsx
+++ app/components/pages/home/know-techs/known-tech.tsx
@@ -1,17 +1,14 @@
 import { CMSIcon } from "@/app/components/cms-icon";
 import { KnownTech as IKnownTech } from "@/app/types/projects";
-// import { getRelativeTimeString } from "@/app/utils/get-relative-time";
 
 type KnownTechProps = {
     tech: IKnownTech
 };
 
 export const KnownTech = ({ tech }: KnownTechProps) => {
-    // const relativeTime = getRelativeTimeString(new Date(tech.startDate), 'pt-BR').replace('há ', '');
     return (
-        <div className="p-6 rounded-lg bg-gray-600/20 text-gray-400 flex flex-col gap-2 hover:text-emerald-500 hover:bg-gray-600/30 transition-all duration-300 group relative flex items-center justify-center">
+        <div className="p-6 rounded-lg bg-gray-600/20 text-gray-400 flex flex-col gap-2 hover:text-emerald-500 hover:bg-gray-600/30 transition-all duration-300 group relative items-center justify-center">
             <div className="flex flex-col items-center justify-between">
-                {/* <p className="font-medium">{tech.name}</p> */}
                 <CMSIcon icon={tech.iconSvg} />
             </div>
 
